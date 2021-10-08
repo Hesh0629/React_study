0f381fe83fb5f56408911deac707fa1c4ec721ec
한성환 <hesh0629@kakao.com>
2021-10-08 14:21:05 +0000

    ref를 통하여 DOM에 이름 달기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,60 +1,74 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import './App.css';
 import HeshComp from './HeshComp';
 import Counter from './Counter';
 import Say from './Say';
 import EventPractice_Class from './EventPractice_Class';
 import EventPractice_Function from './EventPractice_Function';
-import ValidationSample from './ValidationSample'
+import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 function App() {
-	const name = '얘는 스크립트입니다';
-	const if_state = 'if문은 못씁니다';
-	const undef = undefined; // 이대로 undef를 return 때리면 에러 발생
-	/*const inline_style = {
+  const name = '얘는 스크립트입니다';
+  const if_state = 'if문은 못씁니다';
+  const undef = undefined; // 이대로 undef를 return 때리면 에러 발생
+  /*const inline_style = {
 		backgroundColor: '#c0ffee',
 		color: 'black',
 		fontWeight: 'bold',
 		padding: 30, 
 	}; 인라인으로 쓰기 싫어,,, */
-	return (
-		//Fragment 부터 JSX 시작
-		<Fragment>
-			{/*JSX 내부에서는 이렇게 주석 처리해야함*/}
-			<div className="react">
-				<h1>리액트 ㅎㅇ</h1>
-				<h2>JSX 문법은 html과 닮은 부분이 많답니다.</h2>
-				<h3>{name} 중괄호를 넣어서 스크립트를 들고 올 수 있어요.</h3>
-				<h1> 그런데 if문은 못쓴다네요...?</h1>
-				{if_state === 'if' ? (
-					<h2> 쓸 수 있겠냐고</h2>
-				) : (
-					<h2> 네 못씁니다~ 그래서 삼항 연산자를 가져다 써요;;</h2>
-				)}
-				<h1>더 짧게 쓸래요!</h1>
-				{if_state === 'if문은 못씁니다' && <h3> &&를 써가지고 표현 할 수 있어요.</h3>}
-				<h1> undefined 를 바로 return하여 렌더링 하지않기 (에러발생)</h1>
-				{undef || '대신에 ||기호를 이용해서 쓸 수 있어요'}
-			</div>
-		</Fragment>
-	);
+  return (
+    //Fragment 부터 JSX 시작
+    <Fragment>
+      {/*JSX 내부에서는 이렇게 주석 처리해야함*/}
+      <div className="react">
+        <h1>리액트 ㅎㅇ</h1>
+        <h2>JSX 문법은 html과 닮은 부분이 많답니다.</h2>
+        <h3>{name} 중괄호를 넣어서 스크립트를 들고 올 수 있어요.</h3>
+        <h1> 그런데 if문은 못쓴다네요...?</h1>
+        {if_state === 'if' ? (
+          <h2> 쓸 수 있겠냐고</h2>
+        ) : (
+          <h2> 네 못씁니다~ 그래서 삼항 연산자를 가져다 써요;;</h2>
+        )}
+        <h1>더 짧게 쓸래요!</h1>
+        {if_state === 'if문은 못씁니다' && <h3> &&를 써가지고 표현 할 수 있어요.</h3>}
+        <h1> undefined 를 바로 return하여 렌더링 하지않기 (에러발생)</h1>
+        {undef || '대신에 ||기호를 이용해서 쓸 수 있어요'}
+      </div>
+    </Fragment>
+  );
 }
 // HeshComp 컴포넌트를 불러와서 반환
 const HeshComp_f = () => {
-	return <HeshComp name="asdf"> ㅁㄴㅇㄹ </HeshComp>;
+  return <HeshComp name="asdf"> ㅁㄴㅇㄹ </HeshComp>;
 };
 const Counter_f = () => {
-	return <Counter />;
+  return <Counter />;
 };
 const Say_f = () => {
-	return <Say />;
+  return <Say />;
 };
 const EventPractice_Class_f = () => {
-	return <EventPractice_Class />
-}
+  return <EventPractice_Class />;
+};
 const EventPractice_Function_f = () => {
-	return <EventPractice_Function />
-}
+  return <EventPractice_Function />;
+};
 const ValidationSample_f = () => {
-	return <ValidationSample />
+  return <ValidationSample />;
+};
+class ScrollBox_class extends Component {
+  render() {
+    return (
+      <div>
+        {/* 앞으로 this.scrollBox 는 ScrollBox를 가리키게 된다
+            더 나아가 ScrollBox내부에 있는 메서드들을 이용할 수 있다.
+        */}
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>To Bottom</button>
+      </div>
+    );
+  }
 }
-export default ValidationSample_f
+export default ScrollBox_class;
--- my-app/src/ValidationSample.js
+++ my-app/src/ValidationSample.js
@@ -2,45 +2,50 @@ import React, { Component } from 'react';
 import './ValidationSample.css';
 
 class ValidationSample extends Component {
-	state = {
-		password: '',
-		clicked: false,
-		validated: false,
-	};
+  input = React.createRef();
 
-	handleChange = (e) => {
-		this.setState({
-			password: e.target.value,
-		});
-	};
+  state = {
+    password: '',
+    clicked: false,
+    validated: false,
+  };
 
-	handleButtonClick = () => {
-		this.setState({
-			clicked: true,
-			validated: this.state.password === '0000',
-		});
-	};
-	handleOnKeypress = (e) => {
-		if (e.key === 'Enter') {
-			this.setState({
-				clicked: true,
-				validated: this.state.password === '0000',
-			});
-		}
-	};
-	render() {
-		return (
-			<div>
-				<input
-					type="password"
-					value={this.state.password}
-					onChange={this.handleChange}
-					onKeyPress={this.handleOnKeypress}
-					className={this.state.clicked ? (this.state.validated ? 'success' : 'fail') : ''}
-				/>
-				<button onClick={this.handleButtonClick}>validate</button>
-			</div>
-		);
-	}
+  handleChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  handleButtonClick = () => {
+    this.setState({
+      clicked: true,
+      validated: this.state.password === '0000',
+    });
+    this.input.focus();
+  };
+  handleOnKeypress = (e) => {
+    if (e.key === 'Enter') {
+      this.setState({
+        clicked: true,
+        validated: this.state.password === '0000',
+      });
+    }
+    this.input.focus();
+  };
+  render() {
+    return (
+      <div>
+        <input
+          ref={(ref) => (this.input = ref)}
+          type="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          onKeyPress={this.handleOnKeypress}
+          className={this.state.clicked ? (this.state.validated ? 'success' : 'fail') : ''}
+        />
+        <button onClick={this.handleButtonClick}>validate</button>
+      </div>
+    );
+  }
 }
 export default ValidationSample;
