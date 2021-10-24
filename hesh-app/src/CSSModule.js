16642b1cc244163dcc5924a62d24cd42e7e654a2
한성환 <hesh0629@kakao.com>
2021-10-24 10:38:07 +0000

    node-sass, open-color, include-media 추가

--- hesh-app/src/CSSModule.js
+++ hesh-app/src/CSSModule.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import styles from './CSSModule.module.css';
+import styles from './CSSModule.module.scss';
+import classNames from 'classnames/bind';
+//classnames의 bind를 이용하여 미리 클래스를 받아와서 이용
+const cx = classNames.bind(styles);
 const CSSModule = () => {
-  return(
-    {/*백틱 ` 문자로 문자열 안에 JS reference 넣기가 편해짐*/},
-    <div className={`${styles.wrapper} ${styles.inverted}`}>
-      Hello, i'm <span className="something">CSS Module</span>
-    </div>
+  return (
+    {
+      /*
+      백틱 ` 문자로 문자열 안에 JS reference 넣기가 편해짐
+      하지만 classNames를 이용하면 더 간단  
+    */
+    },
+    (
+      <div className={cx('wrapper', 'inverted')}>
+        Hello, i'm <span className="something">CSS Module</span>
+      </div>
+    )
   );
 };
-
 export default CSSModule;
