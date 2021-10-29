15073770f5cc68e0415d1c0b2e0ee8bc6ae040cf
한성환 <hesh0629@kakao.com>
2021-10-29 14:29:31 +0000

    classnames 와 map을 이용하여 일정관리 리스트 추가하기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 const App = () => {
+  //eslint-disable-next-line
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트 기초',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 제작',
+      checked: false,
+    },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
