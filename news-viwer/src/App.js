70500cc4fd989426b8c7724f4cbb7ba5cc26e807
한성환 <hesh0629@kakao.com>
2021-11-07 10:48:57 +0000

    news-api 정책변화로 localhost가 아니면 API이용 불가

---
+++ news-viwer/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      // 불행히도 news-api측에서 localhost가 아니면 사용 못하도록 막아버렸다. 현재 상황에서는 할 수 있는게 없다.
+      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=d81b0e5584414c55bb8e7543639e000b',);
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>load</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
+    </div>
+  );
+};
+
+export default App;
