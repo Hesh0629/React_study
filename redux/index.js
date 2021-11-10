b2a1ca99e1fe88d02be6c614f17a66a1b0ec8e7a
한성환 <hesh0629@kakao.com>
2021-11-10 14:59:02 +0000

    redux로 상태 관리를 편하게 하기.

---
+++ redux/index.js
@@ -0,0 +1,70 @@
+import { createStore } from 'redux';
+
+const divToggle = document.querySelector('.toggle');
+const counter = document.querySelector('h1');
+const btnIncrease = document.querySelector('#increase');
+const btnDecrease = document.querySelector('#decrease');
+
+// 액션 이름은 문자열로 만들어야함
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+// 그 이후, 액션 객체를 만드는 생성 함수 제작
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = difference => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+  toggle: false,
+  counter: 0,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state, // 불변성유지는 꼭 해주자
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+}
+//store 제작 (파라미터로 reducer 넘겨주기)
+const store = createStore(reducer);
+
+const render = () => {
+  const state = store.getState();
+  if (state.toggle) {
+    divToggle.classList.add('active');
+  } else {
+    divToggle.classList.remove('active');
+  }
+  counter.innerHTML = state.counter;
+};
+
+render();
+// 상태가 업데이트될 때 마다 render함수 실행
+store.subscribe(render);
+
+// 짜잔 onClick이 아니라 onclick 이랍니다.
+divToggle.onclick = () => {
+  store.dispatch(toggleSwitch());
+};
+btnIncrease.onclick = () => {
+  store.dispatch(increase(1));
+};
+btnDecrease.onclick = () => {
+  store.dispatch(decrease());
+};
---
+++ redux/readme.md
@@ -0,0 +1,2 @@
+## 제발 설치할 때  npm install @reduxjs/toolkit 이용
+ref: https://ko.redux.js.org/introduction/getting-started/
