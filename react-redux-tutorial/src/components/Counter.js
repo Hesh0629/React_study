ac6b681f0dbbea2bc2434e39a43515ef441a611f
한성환 <hesh0629@kakao.com>
2021-11-13 14:54:02 +0000

    리액트 리덕스 + devTools 사용하기

--- react-redux-tutorial/src/components/Counter.js
+++ react-redux-tutorial/src/components/Counter.js
@@ -1,8 +1,8 @@
 // 프레젠테이셔널 컴포넌트 : 상태 업뎃에는 관여하지 않고 그저 props만 받아와서 화면에 띄움
-import React from 'react'
+import React from 'react';
 
-const Counter = ({ number, onIncrease, onDecrease}) => {
-  return(
+const Counter = ({ number, onIncrease, onDecrease }) => {
+  return (
     <div>
       <h1>{number}</h1>
       <div>
